1a2a078ef42e75f9e2c0af95353874a5294edb55
Francisco Checa <fcheca93@gmail.com>
2026-06-11 18:23:15 +0200

    feat: add null validation in transform

--- src/transform.py
+++ src/transform.py
@@ -3,6 +3,8 @@ import pandas as pd
 def transform(df):
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m')
     df['country_code'] = 'DE'
+    if df['unemployment_rate'].isnull().sum() > 0:
+        print(f"Warning: {df['unemployment_rate'].isnull().sum()} null values in unemployment_rate")
     print(df.dtypes)
     print(df.head())
     print(df.isnull().sum())
